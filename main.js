47ece63c6759e4f4cae2b6585f6fe6d610ad5e0d
Bala Chinnaiah <97093618+BalaChinnaiah@users.noreply.github.com>
2023-06-07 16:34:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+// Log Statement using console log
+console.log('Hello From Main.JS')
+
+// let may be not initialized first and can make changes afterwrds
+let age = 20
+console.log(age)
+
+// const must be initialized and can't be changed afterwards
+
+const salary = 500000
+console.log(salary)
+// DATA TYPES
+// Strings
+const name ='sagar'
+const str = " Hello Bala"
+const str2 = `Hello Js`
+
+console.log(name+str+ " " +  str2)
+
+// Number
+const total=0
+const pi = 3.14
+
+// Boolean
+const isPrimary = true
+const isNewUser = false
+
+// Undefines
+let sum;
+console.log(sum)
+
+const data = null
+//Object
+const user = {
+    firstname:'bala',
+    lastname: ' sagar ',
+    age : 30
+}
+
+console.log(user.firstname,user.age )
+//array
+const arr = [1,3,5,7,9]
+console.log(arr[0])
